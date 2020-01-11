ad94d3e980ed24d6638a773de053c74272ffcbad
Maciej Opaliński <poprostumieciek@gmail.com>
2020-01-11 18:03:25 +0100

    Add command and bot status handler
    Added basic command handler.
    Commands will be loaded from 'commands/' directory.
    
    Added basic bot status handler.
    Statuses will be choosen randomly every 5 seconds.

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
 const Discord = require("discord.js");
+const fs = require("fs");
+
 const bot = new Discord.Client();
-const fs = require('fs');
+
+bot.prefix = "!";
+bot.commands = new Discord.Collection();
+bot.command_details = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if(err) throw err;
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0)
+    {
+        console.log(`ERR: Commands not found!`);
+        return;
+    }
+    jsfiles.forEach((file, index) => {
+        let props = require(`./commands/${file}`);
+        console.log(`INFO: ${file} loaded`);
+        bot.commands.set(props.help.name, props);
+        bot.command_details.set(props.help);
+    });
+});
+
+bot.on('ready', async () => {
+    let status = [
+        {
+            status: "online",
+            game: {
+                name: "Metallica",
+                type: "LISTENING"
+            }
+        },
+        {
+            status: "idle",
+            game: {
+                name: "Visual Studio Code",
+                type: "PLAYING"
+            }
+        }
+    ];
+
+    setInterval(function() {
+        let index = Math.floor(Math.random() * status.length);
+        bot.user.setPresence(status[index]);
+    }, 5000);
+
+    console.log(`INFO: Running...`);
+});
+
+bot.on('message', async msg => {
+    if(msg.author.bot) return;
+    if(msg.channel.type === "dm") return;
+
+    let messageArray = msg.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    let commandfile = bot.commands.get(cmd.slice(bot.prefix.length));
+    if(commandfile)
+    {
+        if(msg.content.startsWith(bot.prefix))
+        {
+            commandfile.run(bot, msg, args);
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN);
