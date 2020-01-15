4f2a1b20600e947d2292fabad6ff7d361b8f5639
Maciej Opaliński <poprostumieciek@gmail.com>
2020-01-15 16:36:49 +0100

    Update clear command

--- commands/clear.js
+++ commands/clear.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, msg, args) => {
     let messages = args[0];
     if(messages >= 2 && messages <= 100)
     {
+        msg.delete();
         msg.channel.bulkDelete(messages).catch(err => {
-            msg.delete(bot.delete_timeout);
             msg.channel.send(err.message).then(msg => msg.delete(bot.delete_timeout));
         });
     }
