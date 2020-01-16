37ce7a9bce1f081768789783ae78829c9ed14991
Maciej Opaliński <poprostumieciek@gmail.com>
2020-01-16 20:26:47 +0100

    Add info command

---
+++ commands/info.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const package = require("../package.json");
+
+module.exports.run = async (bot, msg, args) => {
+    if(args[0] == "server")
+    {
+        let message = new Discord.RichEmbed()
+        .setTitle(`INFO: ${msg.guild.name}`)
+        .addField(`Owner:`, `${msg.guild.owner.user.tag}`)
+        .addField(`Created at:`, `${msg.guild.owner.joinedAt.toDateString()}`);
+
+        msg.channel.send(message);
+    }
+    else if(args[0] == "bot")
+    {
+        let message = new Discord.RichEmbed()
+        .setTitle(`INFO: MieciekBot`)
+        .addField(`Author:`, `PoProstuMieciek`)
+        .addField(`Version:`, `${package.version}`)
+        .addField(`GitHub repository:`, `https://github.com/PoProstuMieciek/MieciekBot`);
+
+        msg.channel.send(message);
+    }
+    else
+    {
+        msg.delete(bot.delete_timeout);
+    }
+}
+
+module.exports.help = {
+    name: "info",
+    args: [
+        "<server/bot>"
+    ],
+    permission: "USER",
+    description: "prints info about server or bot"
+}
+
+module.exports.error = {}
