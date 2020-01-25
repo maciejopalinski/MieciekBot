8aed1486458dcd528192faef1085735b4da43e94
Maciej Opaliński <poprostumieciek@gmail.com>
2020-01-25 16:46:58 +0100

    Add avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, msg, args) => {
+    let user = msg.mentions.users.first() || msg.author;
+
+    let avatar_embed = new Discord.RichEmbed()
+    .setTitle(`AVATAR: ${user.username}`)
+    .setImage(user.avatarURL);
+
+    msg.channel.send(avatar_embed);
+}
+
+module.exports.help = {
+    name: "avatar",
+    args: [],
+    permission: "USER",
+    description: "shows user avatar"
+}
+
+module.exports.error = {
+}
