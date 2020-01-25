411952f3b3f43ee331692a284063d1f3fb6f66fc
Maciej Opaliński <poprostumieciek@gmail.com>
2020-01-25 16:18:18 +0100

    Update info command
    Added thumbnails and new informations to info embeds.

--- commands/info.js
+++ commands/info.js
@@ -6,8 +6,10 @@ module.exports.run = async (bot, msg, args) => {
     {
         let message = new Discord.RichEmbed()
         .setTitle(`INFO: ${msg.guild.name}`)
+        .setThumbnail(`${msg.guild.iconURL}`)
         .addField(`Owner:`, `${msg.guild.owner.user.tag}`)
-        .addField(`Created at:`, `${msg.guild.owner.joinedAt.toDateString()}`);
+        .addField(`Created at:`, `${msg.guild.owner.joinedAt.toDateString()}`)
+        .addField(`Members:`, `${msg.guild.memberCount}`)
 
         msg.channel.send(message);
     }
@@ -15,6 +17,7 @@ module.exports.run = async (bot, msg, args) => {
     {
         let message = new Discord.RichEmbed()
         .setTitle(`INFO: MieciekBot`)
+        .setThumbnail(`https://cdn.discordapp.com/avatars/510925936393322497/15784b2d9cf8df572617b493bc79c707.png?size=4096`)
         .addField(`Author:`, `PoProstuMieciek`)
         .addField(`Version:`, `${package.version}`)
         .addField(`GitHub repository:`, `${package.repository.url}`);
