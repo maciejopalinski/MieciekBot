41354aa9538ae8283b812b6e98a1fd4b7a2f458a
Maciej Opaliński <poprostumieciek@gmail.com>
2020-02-13 19:58:44 +0100

    Add new subreddits to meme command
    Added new subreddits to meme command:
    * r/cursedimages
    * r/blessedimages
    * r/blursedimages
    * r/ihadastroke

--- commands/meme.js
+++ commands/meme.js
@@ -2,7 +2,15 @@ const Discord = require("discord.js");
 const RandomPuppy = require("random-puppy");
 
 module.exports.run = async (bot, msg, args) => {
-    let subReddits = ["dankmeme", "meme", "me_irl"];
+    let subReddits = [
+        "dankmeme",
+        "meme",
+        "me_irl",
+        "cursedimages",
+        "blessedimages",
+        "blursedimages",
+        "ihadastroke"
+    ];
     
     let random = subReddits[Math.floor(Math.random() * subReddits.length)];
 
@@ -13,7 +21,7 @@ module.exports.run = async (bot, msg, args) => {
     .setURL(`https://reddit.com/r/${random}`)
     .setFooter(`Powered by MieciekBot ${bot.settings.version}`, bot.settings.iconURL);
 
-    msg.channel.send(meme_embed);
+    return msg.channel.send(meme_embed);
 }
 
 module.exports.help = {
