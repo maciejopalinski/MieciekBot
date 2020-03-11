c069bbc5be935f3084ebea590bfa50e6f7aae65d
Maciej Opaliński <poprostumieciek@gmail.com>
2020-03-11 22:17:05 +0100

    Add random command

---
+++ commands/random.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, msg, args) => {
+    let range = parseInt(args[0]);
+
+    if(range > 1 && range <= 65535)
+    {
+        let random = Math.floor(Math.random() * range) + 1;
+        
+        let random_embed = new Discord.RichEmbed()
+        .setTitle(`Random: 1 - ${range}`)
+        .addField(`Random number from 1 to ${range}:`, random)
+        .setFooter(`Powered by MieciekBot ${bot.settings.version}`, bot.settings.iconURL);
+
+        msg.channel.send(random_embed);
+    }
+    else
+    {
+        msg.delete(bot.delete_timeout);
+        msg.channel.send(this.error.cannot_roll).then(msg => msg.delete(bot.delete_timeout));
+    }
+}
+
+module.exports.help = {
+    name: "random",
+    aliases: [
+        "roll",
+        "dice",
+        "roll-dice",
+        "roll_dice",
+        "rand",
+        "srand"
+    ],
+    args: [
+        "<range>"
+    ],
+    permission: "USER",
+    description: "rolls dice with <range> sides"
+}
+
+module.exports.error = {
+    "cannot_roll": "Range must be in range *2 - 65535*. Please, pick different number and try again."
+}
