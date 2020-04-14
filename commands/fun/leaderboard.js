6f0bae152aae5c93cc12a1b8585efe5743c98bb3
Maciej Opaliński <poprostumieciek@gmail.com>
2020-04-14 18:54:15 +0200

    Add 'leaderboard' command

---
+++ commands/fun/leaderboard.js
@@ -0,0 +1,63 @@
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+
+const Users = require("../../models/users.js");
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/**
+ * @param {Discord.Client} bot 
+ * @param {Discord.Message} msg 
+ * @param {Array<String>} args 
+ */
+module.exports.run = async (bot, msg, args) => {
+    Users.find({
+        serverID: msg.guild.id
+    }).sort([
+        ['xp', 'descending']
+    ]).exec((err, res) => {
+        if (err) console.error(err);
+
+        let rank_embed = new Discord.RichEmbed()
+        .setTitle(`${msg.guild.name} Leaderboard`)
+        .setFooter(`Powered by MieciekBot ${bot.settings.version}`, bot.settings.iconURL);
+
+        let new_res = [];
+        res.forEach(elem => {
+            if(elem.xp != 0) new_res.push(elem);
+        });
+        res = new_res;
+
+        if(res.length == 0)
+        {
+            rank_embed.addField(`There are not any members in database.`, '\u200b');
+        }
+        else if(res.length < 10)
+        {
+            res.forEach((elem, index) => {
+                rank_embed.addField(`${index+1}. @${msg.guild.members.get(elem.userID).user.tag}`, `${elem.xp} XP`);
+            });
+        }
+        else if(res.length > 10)
+        {
+            for (let i = 1; i <= 10; i++)
+            {
+                let elem = res[i];
+                rank_embed.addField(`${index}. @${msg.guild.members.get(elem.userID).user.tag}`, `${elem.xp} XP`);
+            }
+        }
+
+        msg.channel.send(rank_embed);
+    });
+}
+
+module.exports.help = {
+    name: "leaderboard",
+    aliases: [],
+    args: [],
+    permission: "USER",
+    description: "displays server activity leaderboard"
+}
