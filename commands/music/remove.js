9137ead21e3144914dda61a76e2365d1e2df80c3
Maciej Opaliński <poprostumieciek@gmail.com>
2020-04-18 20:02:29 +0200

    Add 'remove' command

---
+++ commands/music/remove.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+
+/**
+ * @param {Discord.Client} bot 
+ * @param {Discord.Message} msg 
+ * @param {Array<String>} args 
+ */
+module.exports.run = async (bot, msg, args) => {
+    let queue = bot.queue;
+    let server_queue = bot.queue.get(msg.guild.id);
+
+    args[0] = parseInt(args[0]);
+    if(args[0] <= 0 || args[0] > server_queue.songs.length - 1)
+    {
+        msg.delete(bot.delete_timeout);
+        return msg.channel.send(this.error.invalid_num).then(msg => msg.delete(bot.delete_timeout));
+    }
+
+    if(server_queue)
+    {
+        msg.channel.send(this.error.removed.replace("{{pos}}", args[0]));
+        server_queue.songs.splice(args[0], 1);
+    }
+    else
+    {
+        msg.delete(bot.delete_timeout);
+        return msg.channel.send(this.error.must_play).then(msg => msg.delete(bot.delete_timeout));
+    }
+}
+
+module.exports.help = {
+    name: "remove",
+    aliases: [],
+    args: [
+        "<position>"
+    ],
+    permission: "DJ",
+    description: "removes track from queue"
+}
+
+module.exports.error = {
+    "must_play": "There must be something in queue to remove it.",
+    "removed": "Track {{pos}} removed from the queue.",
+    "invalid_num": "Invalid number specified."
+}
