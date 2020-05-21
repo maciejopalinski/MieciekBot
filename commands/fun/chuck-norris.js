c37ac98edf8bfaa6e6b1db3399251572e2225fdb
Maciej Opaliński <poprostumieciek@gmail.com>
2020-05-21 22:49:40 +0200

    Add chuck-norris command

---
+++ commands/fun/chuck-norris.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const Request = require("request");
+
+/**
+ * @param {Discord.Client} bot 
+ * @param {Discord.Message} msg 
+ * @param {Array<String>} args 
+ */
+module.exports.run = async (bot, msg, args) => {
+    var options = {
+        method: 'GET',
+        url: 'https://api.chucknorris.io/jokes/random/'
+    };
+
+    Request(options, async (err, res, body) => {
+        if(err) return console.error(err);
+        body = JSON.parse(body);
+
+        const joke_embed = new Discord.RichEmbed()
+        .setTitle('Chuck Norris Joke')
+        .setURL(body.url)
+        .addField(body.value, '\u200b')
+        .setFooter(`Powered by MieciekBot ${bot.settings.version}`, bot.settings.iconURL);
+
+        msg.channel.send(joke_embed);
+    });
+}
+
+module.exports.help = {
+    name: "chuck-norris",
+    aliases: [
+        "chuck",
+        "norris",
+        "cnjoke"
+    ],
+    args: [],
+    permission: "USER",
+    description: "displays random Chuck Norris joke"
+}
