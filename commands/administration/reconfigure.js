2f7ccdc4f074675da989d04b07eee1e166565941
Maciej Opaliński <poprostumieciek@gmail.com>
2020-05-22 19:53:46 +0200

    Simplify code in reconfigure command

--- commands/administration/reconfigure.js
+++ commands/administration/reconfigure.js
@@ -12,61 +12,10 @@ const Warns = require("../../models/warns.js");
 module.exports.run = async (bot, msg, args) => {    
     if(args[0] == `confirm${msg.guild.id}`)
     {
-        msg.channel.send(`Reseting...`).then(msg => msg.delete(bot.delete_timeout));
+        await msg.channel.send(`Reseting...`).then(msg => msg.delete(bot.delete_timeout));
 
-        Servers.findOneAndDelete({
-            serverID: msg.guild.id
-        }, err => {
-            if(err) console.error(err);
-        });
-    
-        Users.deleteMany({
-            serverID: msg.guild.id
-        }, err => {
-            if(err) console.error(err);
-        });
-    
-        Warns.deleteMany({
-            serverID: msg.guild.id
-        }, err => {
-            if(err) console.error(err);
-        });
-        
-        const new_server = new Servers({
-            serverID: msg.guild.id,
-            prefix: "!",
-            delete_timeout: 3000,
-            roles: {
-                owner: "",
-                admin: "",
-                dj: "",
-                user: "",
-                mute: ""
-            },
-            spam_channels: []
-        });
-        new_server.save().catch(err => console.error(err));
-    
-        let guild_members = [];
-        msg.guild.members.forEach(member => {
-            if(!member.user.bot)
-            {
-                guild_members.push({
-                    serverID: member.guild.id,
-                    userID: member.id,
-                    level: 0,
-                    xp: 0
-                });
-            }
-        });
-    
-        Users.insertMany(guild_members, err => {
-            if(err) console.error(err);
-        });
-    
-        msg.guild.owner.send(`Hi! I just configured your server. Please, set up all required permissions, roles and other useful properties. Have a good time!`).catch(err => {
-            if (err) guild.leave();
-        });
+        bot.emit("guildDelete", msg.guild);
+        bot.emit("guildCreate", msg.guild);
     }
     else
     {
