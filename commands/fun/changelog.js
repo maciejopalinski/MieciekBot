34d4348a8e730b4805aed3eb3c0dc2d9f9e170e9
Maciej Opaliński <poprostumieciek@gmail.com>
2020-11-13 21:00:08 +0100

    Fix changelog command

--- commands/fun/changelog.js
+++ commands/fun/changelog.js
@@ -1,6 +1,6 @@
 const {Client, Message, MessageEmbed} = require('../../lib/mieciekbot.js');
-// const GitHub = require('octonode');
-// const GitHubClient = GitHub.client(process.env.GITHUB_API_TOKEN);
+const GitHub = require('octonode');
+const GitHubClient = GitHub.client(process.env.GITHUB_API_TOKEN);
 
 /**
  * @param {Client} bot 
@@ -8,19 +8,19 @@ const {Client, Message, MessageEmbed} = require('../../lib/mieciekbot.js');
  * @param {Array<String>} args 
  */
 module.exports.run = async (bot, msg, args) => {
-    // let repository = GitHubClient.repo('PoProstuMieciek/MieciekBot');
-    // repository.releases((err, res) => {
-    //     if(err) console.error(err);
-    //     let latest = res[0];
+    let repository = GitHubClient.repo('PoProstuMieciek/MieciekBot');
+    repository.releases((err, res) => {
+        if(err) console.error(err);
+        let latest = res[0];
 
-    //     let changelog_embed = new MessageEmbed()
-    //     .setTitle('CHANGELOG: MieciekBot')
-    //     .setURL(latest.html_url)
-    //     .addField(latest.name, `Released by ${latest.author.login}`)
-    //     .addField('Notes', latest.body.split('#').join(''));
+        let changelog_embed = new MessageEmbed(bot, msg.guild)
+        .setTitle('CHANGELOG: MieciekBot')
+        .setURL(latest.html_url)
+        .addField(latest.name, `Released by ${latest.author.login}`)
+        .addField('Notes', latest.body.split('#').join(''));
 
-    //     msg.channel.send(changelog_embed);
-    // });
+        msg.channel.send(changelog_embed);
+    });
 }
 
 module.exports.help = {
