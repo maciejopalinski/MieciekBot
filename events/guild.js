3dc08e911115cb57683aca28c846d1e7e908675a
Maciej Opaliński <poprostumieciek@gmail.com>
2020-11-19 11:50:46 +0100

    Fix bug when joining a server

--- events/guild.js
+++ events/guild.js
@@ -1,9 +1,9 @@
 const {bot} = require('../index.js');
 
-bot.on('guildCreate', (guild) => {
-    bot.db_manager.defaultServer(guild.id).save().catch(err => console.error(err));
+bot.on('guildCreate', async guild => {
+    await bot.db_manager.defaultServer(guild.id).save().catch(err => console.error(err));
     
-    let owner = bot.users.cache.get(guild.ownerID);
+    let owner = await bot.users.fetch(guild.ownerID);
     if(!owner) return guild.leave();
 
     owner.send(`Hi! I just configured your '${guild.name}' server. Please, set up all required permissions, roles and other useful properties. Have a good time!`).catch(err => {
