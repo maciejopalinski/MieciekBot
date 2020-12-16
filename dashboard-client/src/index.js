fbd3aedc9923032e711e77e7c01000e70aa96f2f
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-16 21:43:52 +0100

    Install Chakra UI

--- dashboard-client/src/index.js
+++ dashboard-client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
