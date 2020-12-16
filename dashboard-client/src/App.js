48f02dac3c586102adf57c61dcfa291c1b749ed3
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-16 21:44:08 +0100

    Create a basic App.js

--- dashboard-client/src/App.js
+++ dashboard-client/src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import './App.css';
+
+import { Button } from '@chakra-ui/react';
 
 function App() {
 	return (
-		<div></div>
+		<div className='app'>
+			<Button colorScheme='blue'>Button</Button>
+		</div>
 	);
 }
 
