60f2db9a855412d621a99931b30dbc8fe52eb8c1
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-17 22:16:08 +0100

    Add user and mutual guilds endpoint
    /api/discord/@me
    /api/discord/guilds/mutual

--- dashboard-api/src/routes/discord.js
+++ dashboard-api/src/routes/discord.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
+const error = require('../templates/ErrorPages');
 
-// /api/discord
-router.get('/', (req, res) => {
-    res.send({
-        message: 'Discord'
-    });
+const User = require('../database/schemas/User');
+const { getBotGuilds, getMutualGuilds } = require('../utils/api');
+
+// /api/discord/@me
+router.get('/@me', (req, res) => {
+    if(req.user) res.send(req.user);
+    else res.status(401).send({ message: "Unauthorized" });
+});
+
+// /api/discord/guilds/mutual
+router.get('/guilds/mutual', async (req, res) => {
+    if(req.user) {
+        const botGuilds = await getBotGuilds();
+        const user = await User.findOne({ id: req.user.id });
+
+        const mutualGuilds = getMutualGuilds(botGuilds, user.guilds);
+        res.send(mutualGuilds);
+    }
+    else error.unauthorized(res);
 });
 
 module.exports = router;
