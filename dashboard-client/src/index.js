365495c09df045a07d0206a9d01f25c05124fcbd
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-18 22:14:12 +0100

    Replace ChakraUI with Bootstrap

--- dashboard-client/src/index.js
+++ dashboard-client/src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
-
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ChakraProvider>
-			<Router>
-				<App />
-			</Router>
-		</ChakraProvider>
-	</React.StrictMode>,
+	// <React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	// </React.StrictMode>
+	,
 	document.getElementById('root')
 );
 
