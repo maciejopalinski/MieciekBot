971075612e580f9cbd7192cbc2e7b8adeb84aaf6
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-18 22:19:11 +0100

    Protect /dashboard and /menu from not logged in users

--- dashboard-client/src/pages/DashboardPage/index.jsx
+++ dashboard-client/src/pages/DashboardPage/index.jsx
@@ -1,7 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
+import { getUserDetails, getMutualGuilds } from '../../util/api';
+
+import '../styles.css';
+import { Navbar } from '../../components';
+// import Formik from 'formik';
 
 export function DashboardPage(props) {
+    
+    const [ user, setUser ] = React.useState({});
+    const [ guilds, setGuilds ] = React.useState([]);
+
+    React.useEffect(() => {
+        getUserDetails()
+        .then(res => {
+            // console.log(res.data);
+            setUser(res.data);
+            
+            return getMutualGuilds();
+        })
+        .then(res => {
+            // console.log(res.data);
+            setGuilds(res.data);            
+        })
+        .catch(err => {
+            props.history.push('/');
+        });
+    }, []);
+
     return (
-        <h1>Dashboard Page</h1>
+        <div>
+            <Navbar user={user} guilds={guilds} />
+            
+            <div className='app-header'>
+                <h1>Dashboard Page</h1>
+            </div>
+        </div>
     );
 }
--- dashboard-client/src/pages/MenuPage/index.jsx
+++ dashboard-client/src/pages/MenuPage/index.jsx
@@ -1,7 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
+import { getUserDetails, getMutualGuilds } from '../../util/api';
+
+import '../styles.css';
+import { Navbar } from '../../components';
 
 export function MenuPage(props) {
+    
+    const [ user, setUser ] = React.useState(null);
+    const [ guilds, setGuilds ] = React.useState([]);
+
+    React.useEffect(() => {
+        getUserDetails()
+        .then(res => {
+            // console.log(res.data);
+            setUser(res.data);
+            
+            return getMutualGuilds();
+        })
+        .then(res => {
+            // console.log(res.data);
+            setGuilds(res.data);
+        })
+        .catch(err => {
+            props.history.push('/');
+        });
+    }, []);
+
     return (
-        <h1>Menu Page</h1>
+        <div>
+            <Navbar user={user} guilds={guilds} />
+            
+            <div className='app-header'>
+                <h1>Menu Page</h1>
+            </div>
+        </div>
     );
 }
