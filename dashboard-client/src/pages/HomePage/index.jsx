909221284f2fba449ae8e9fd8cd4f09a40222d90
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-18 22:19:36 +0100

    Add simple HomePage layout

--- dashboard-client/src/pages/HomePage/index.jsx
+++ dashboard-client/src/pages/HomePage/index.jsx
@@ -1,11 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
-import { Button } from '@chakra-ui/react';
+import { getUserDetails, getMutualGuilds } from '../../util/api';
+
+import '../styles.css';
+import { Navbar } from '../../components';
 
 export function HomePage(props) {
+    
+    const [ user, setUser ] = React.useState(null);
+    const [ guilds, setGuilds ] = React.useState([]);
+
+    React.useEffect(() => {
+        getUserDetails()
+        .then(res => {
+            // console.log(res.data);
+            setUser(res.data);
+            
+            return getMutualGuilds();
+        })
+        .then(res => {
+            // console.log(res.data);
+            setGuilds(res.data);
+        })
+        .catch(err => {
+            console.log("[INFO] Not logged in");
+            setUser({});
+        });
+    }, []);
+
     return (
         <div>
-            <h1>Home Page</h1>
-            <Button colorScheme='blue'>Login</Button>
+            <Navbar user={user} guilds={guilds} />
+            
+            <div className='app-header'>
+                <img
+                    className='mieciekbot-img'
+                    src='https://cdn.discordapp.com/avatars/511219391539445761/067aec30675c8483654c344bfabd2c19.png?size=4096'
+                    alt='MieciekBot Avatar'
+                    width='400px'
+                />
+                <h1>This is the best Discord Bot!</h1>
+                <h3>Includes: moderation, leveling, music and much more!</h3>
+            </div>
         </div>
     );
 }
