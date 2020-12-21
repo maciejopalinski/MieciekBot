43a56ad6e2a351c8d3c617e45ccbbcbef994fa63
Maciej Opaliński <poprostumieciek@gmail.com>
2020-12-21 19:07:37 +0100

    Change design of HomePage

--- dashboard-client/src/pages/HomePage/index.jsx
+++ dashboard-client/src/pages/HomePage/index.jsx
@@ -1,46 +1,37 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
-import { getUserDetails, getMutualGuilds } from '../../util/api';
+import { getUserDetails } from '../../util/api';
 
-import '../styles.css';
+import './styles.css';
 import { Navbar } from '../../components';
 
 export function HomePage(props) {
     
     const [ user, setUser ] = React.useState(null);
-    const [ guilds, setGuilds ] = React.useState([]);
 
     React.useEffect(() => {
+
         getUserDetails()
-        .then(res => {
-            // console.log(res.data);
-            setUser(res.data);
-            
-            return getMutualGuilds();
-        })
-        .then(res => {
-            // console.log(res.data);
-            setGuilds(res.data);
-        })
+        .then(res => setUser(res.data))
         .catch(err => {
-            console.log("[INFO] Not logged in");
             setUser({});
+            console.log("[INFO] Not logged in");
         });
+
     }, []);
 
     return (
         <div>
-            <Navbar user={user} guilds={guilds} />
+            <Navbar user={user} />
             
-            <div className='app-header'>
+            <div className='app'>
                 <img
                     className='mieciekbot-img'
                     src='https://cdn.discordapp.com/avatars/511219391539445761/067aec30675c8483654c344bfabd2c19.png?size=4096'
                     alt='MieciekBot Avatar'
-                    width='400px'
                 />
-                <h1>This is the best Discord Bot!</h1>
-                <h3>Includes: moderation, leveling, music and much more!</h3>
+                <h1 className='mieciekbot-name'>MieciekBot</h1>
+                <p className='description'>Includes moderation, leveling, music and much more!</p>
             </div>
         </div>
     );
