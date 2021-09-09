040baaee51f7ec1827151438a6883d5fc1727e82
Maciej Opaliński <poprostumieciek@gmail.com>
2021-09-09 18:11:15 +0200

    Add proper client intents

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
+import { Intents } from 'discord.js';
 import { Client } from './lib';
 
-export const client = new Client({ ws: {
+export const client = new Client({
     intents: [
-        'GUILDS',
-        'GUILD_MEMBERS',
-        'GUILD_MESSAGES',
-        'GUILD_MESSAGE_REACTIONS',
-        'GUILD_EMOJIS',
-        'GUILD_VOICE_STATES'
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
-}});
+});
 
-client.token = process.env.BOT_TOKEN;
+client.token = process.env.DISCORD_TOKEN;
 client.db_uri = process.env.DATABASE;
 
 client.init();
