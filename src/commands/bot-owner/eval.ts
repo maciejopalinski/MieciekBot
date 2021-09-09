638c2ea284a8161614a5dcf29f2d65b833391a89
Maciej Opaliński <poprostumieciek@gmail.com>
2021-09-09 18:17:09 +0200

    Rework `bot-owner/eval.ts` command

--- src/commands/bot-owner/eval.ts
+++ src/commands/bot-owner/eval.ts
@@ -1,35 +1,59 @@
-import { Command } from '../../lib';
+import { Command, CommandType } from '../../lib';
 import { inspect } from 'util';
 import { MessageAttachment } from 'discord.js';
 
-const Eval = new Command();
+const Eval = new Command({
+    type: CommandType.BOTH,
+    aliases: [],
+    permission: 'BOT_OWNER'
+});
 
-Eval.execute = async (bot, msg, args) => {
+Eval.data
+    .setName('eval')
+    .setDescription('evaluates given JavaScript expression')
+    .addStringOption(opt =>
+        opt
+            .setName('expression')
+            .setDescription('JavaScript expression to evaluate')
+            .setRequired(true)
+    );
+
+Eval.executeFromMessage = async (bot, msg, args) => {
+
+    const code = args.join(' ');
+    const output = evaluate(code);
+
+    msg.channel.send({ files: [new MessageAttachment(Buffer.from(output), 'output.txt')] });
+}
+
+Eval.executeFromInteraction = async (bot, interaction) => {
+
+    await interaction.deferReply();
+    const code = interaction.options.getString('expression');
+    const output = evaluate(code);
+
+    interaction.editReply({ files: [new MessageAttachment(Buffer.from(output), 'output.txt')] });
+}
+
+const evaluate = (code: string): string | any => {
+
     try {
-        const code = args.join(' ');
+        // jailed inside this function! cannot access bot, msg, interaction, etc.
         let evaled = eval(code);
-
         if (typeof evaled !== 'string') evaled = inspect(evaled);
-        let clean_out = clean(evaled);
 
-        msg.channel.send(new MessageAttachment(Buffer.from(clean_out), 'output.txt'));
+        return clean(evaled);
+    }
+    catch (err) {
+        if (typeof err !== 'string') err = inspect(err);
 
-    } catch (err) {
-        msg.channel.send(`${clean(err)}`, { code: 'xl' });
+        return clean(err);
     }
 }
 
-const clean = (text) => {
-    if (typeof(text) === 'string') return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+const clean = (text: any): string | any => {
+    if (typeof text === 'string') return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
     else return text;
 }
-
-Eval.help = {
-    name: 'eval',
-    args: '<expression>',
-    aliases: [],
-    description: 'evaluates given expression',
-    permission: 'BOT_OWNER'
-};
 
 export default Eval;
