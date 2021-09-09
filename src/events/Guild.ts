364c32fdca0edf24e9f7f335cf70cd7f3ceffb41
Maciej Opaliński <poprostumieciek@gmail.com>
2021-09-09 18:12:07 +0200

    Update `Guild.ts` events

--- src/events/Guild.ts
+++ src/events/Guild.ts
@@ -5,15 +5,19 @@ import * as Models from '../models';
 export const onGuildCreate = async (client: Client, guild: Guild) => {
     await Models.Guild.create({ guildID: guild.id }).catch(err => console.error(err));
 
-    let owner = await client.users.fetch(guild.ownerID);
-    if(!owner) return guild.leave();
+    let owner = await client.users.fetch(guild.ownerId);
+    if(!owner)
+    {
+        guild.leave();
+        return;
+    }
 
     let owner_embed = new MessageEmbed(client, guild)
     .setTitle(guild.name)
     .addField('Hi! I just configured your server.', 'Please, set up all required permissions, roles and other useful properties.')
     .addField('\u200b', 'Have a good time!');
 
-    owner.send(owner_embed).catch(err => {
+    owner.send({ embeds: [owner_embed] }).catch(err => {
         if(err) return console.error(err);
     });
 
@@ -27,6 +31,6 @@ export const onGuildDelete = async (client: Client, guild: Guild) => {
 }
 
 export default (client: Client) => {
-    client.on('guildCreate', async guild => onGuildCreate(client, guild));
-    client.on('guildDelete', async guild => onGuildDelete(client, guild));
+    client.on('guildCreate', guild => onGuildCreate(client, guild));
+    client.on('guildDelete', guild => onGuildDelete(client, guild));
 }
